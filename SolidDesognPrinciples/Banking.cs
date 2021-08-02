7e9e5843deb5b1de714ebf74ee2be2a1dadee16b
VivekG11 <vivekvk2903@gmail.com>
2021-08-03 00:40:21 +0530

    [Implementing Single Responsiblity Principle]

---
+++ SolidDesognPrinciples/Banking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidDesognPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SolidDesognPrinciples/TransactionType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidDesognPrinciples
+{
+    public class TransactionType : Opertion
+    {
+        public double balance = 10000;
+      
+        public void Deposit(int amount)
+        {
+          
+            if (amount > 0)
+            {
+                balance += amount;
+            }     
+             if(amount < 0)
+            {
+                throw new InvalidOperationException();
+            }
+          
+        }
+        public void Withdraw(int amount)
+        {
+            if (amount > balance)
+                throw new InvalidOperationException();
+             if(amount < balance)
+            balance = balance - amount;
+        }
+    }
+}
